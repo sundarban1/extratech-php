d8013dad0b313611da94f0f22b85bd056fd03a19
sundarban <sundar.ban@bigtincan.com>
2022-02-23 18:43:13 +1100

    username validation

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php include 'inc/header.php' ?>
 
 <?php
-if (isset($_POST['register'])) {    
+if (isset($_POST['register'])) {
 
   $errorMsg = '';
 
@@ -11,81 +11,80 @@ if (isset($_POST['register'])) {
   $mobile = $_POST['mobile'];
   $password = $_POST['password'];
 
-  if (!preg_match("/^[a-zA-z]*$/", $name) ) {  
-    $errorMsg = "Your name is not valid.";  
-             echo $ErrMsg;  
-} 
+  if (empty($name) || !preg_match("/^[a-zA-z]*$/", $name)) {
+    $errorMsg = "Your name is empty or invalid.";
+  } elseif (empty($username) || !ctype_alnum($username)) {
+    $errorMsg = "Your username is empty or invalid.";
   }
+}
 ?>
 
 <div class="container">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark card-header">
+    <a class="navbar-brand" href="index.php"><i class="fas fa-home mr-2"></i>Dashboard</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav ml-auto">
 
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark card-header">
-        <a class="navbar-brand" href="index.php"><i class="fas fa-home mr-2"></i>Dashboard</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul class="navbar-nav ml-auto">
-          
-              <li class="nav-item
+        <li class="nav-item
 
                active ">
-                <a class="nav-link" href="register.php"><i class="fas fa-user-plus mr-2"></i>Register</a>
-              </li>
-              <li class="nav-item
+          <a class="nav-link" href="register.php"><i class="fas fa-user-plus mr-2"></i>Register</a>
+        </li>
+        <li class="nav-item
                 ">
-                <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt mr-2"></i>Login</a>
-              </li>
+          <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt mr-2"></i>Login</a>
+        </li>
 
-          </ul>
+      </ul>
 
-        </div>
-      </nav>
+    </div>
+  </nav>
 
- <div class="card ">
-   <div class="card-header">
-          <h3 class='text-center'>User Registration</h3>
-          <?php if (!empty($errorMsg)){
-          ?>
-                <p class="text-center"><?php echo $errorMsg; ?></p>
-            <?php
-          }?>
-        </div>
-        <div class="cad-body">
-            <div style="width:600px; margin:0px auto">
+  <div class="card ">
+    <div class="card-header">
+      <h3 class='text-center'>User Registration</h3>
+      <?php if (!empty($errorMsg)) {
+      ?>
+        <p class="text-center"><?php echo $errorMsg; ?></p>
+      <?php
+      } ?>
+    </div>
+    <div class="cad-body">
+      <div style="width:600px; margin:0px auto">
 
-            <form class="" action="register.php" method="post">
-                <div class="form-group pt-3">
-                  <label for="name">Your name</label>
-                  <input type="text" name="name"  class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="username">Your username</label>
-                  <input type="text" name="username"  class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="email">Email address</label>
-                  <input type="email" name="email"  class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="mobile">Mobile Number</label>
-                  <input type="text" name="mobile"  class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" name="password" class="form-control">
-                  <input type="hidden" name="roleid" value="3" class="form-control">
-                </div>
-                <div class="form-group">
-                  <button type="submit" name="register" class="btn btn-success">Register</button>
-                </div>
-            </form>
+        <form class="" action="register.php" method="post">
+          <div class="form-group pt-3">
+            <label for="name">Your name</label>
+            <input type="text" name="name" class="form-control">
           </div>
-
-        </div>
+          <div class="form-group">
+            <label for="username">Your username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" name="email" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="mobile">Mobile Number</label>
+            <input type="text" name="mobile" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control">
+            <input type="hidden" name="roleid" value="3" class="form-control">
+          </div>
+          <div class="form-group">
+            <button type="submit" name="register" class="btn btn-success">Register</button>
+          </div>
+        </form>
       </div>
 
-      <?php include 'inc/footer.php' ?>
+    </div>
+  </div>
+
+  <?php include 'inc/footer.php' ?>
