0ff2c8260ceefdba4466150074d8b87724138744
sundarban <sundar.ban@bigtincan.com>
2022-02-23 19:27:37 +1100

    navbar separted

--- register.php
+++ register.php
@@ -2,6 +2,9 @@
 
 <?php
 if (isset($_POST['register'])) {
+
+  $errorMsg = '';
+
   $name = trim($_POST['name']);
   $username = $_POST['username'];
   $email = $_POST['email'];
@@ -27,29 +30,11 @@ if (isset($_POST['register'])) {
 ?>
 
 <div class="container">
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark card-header">
-    <a class="navbar-brand" href="index.php"><i class="fas fa-home mr-2"></i>Dashboard</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<?php
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav ml-auto">
+include 'inc/navbar.php';
 
-        <li class="nav-item
-
-               active ">
-          <a class="nav-link" href="register.php"><i class="fas fa-user-plus mr-2"></i>Register</a>
-        </li>
-        <li class="nav-item
-                ">
-          <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt mr-2"></i>Login</a>
-        </li>
-
-      </ul>
-
-    </div>
-  </nav>
+?>
 
   <div class="card ">
     <div class="card-header">
