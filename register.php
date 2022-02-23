9f14d97f4ad00e110ae85eed441e0f1f91096e99
Bina Balami <binabalami@Binas-MacBook-Air.local>
2022-02-23 18:15:43 +1100

    updated something

--- register.php
+++ register.php
@@ -1,3 +1,76 @@
-<?php
-echo 'register';
+<?php include 'inc/header.php' ?>
+
+<?php 
+
+if (issey($_POst['register'])){
+    print_r($_POST['name']);
+    exit;
+}
 ?>
+
+
+<div class="container">
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark card-header">
+        <a class="navbar-brand" href="index.php"><i class="fas fa-home mr-2"></i>Dashboard</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav ml-auto">
+          
+              <li class="nav-item
+
+               active ">
+                <a class="nav-link" href="register.php"><i class="fas fa-user-plus mr-2"></i>Register</a>
+              </li>
+              <li class="nav-item
+                ">
+                <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt mr-2"></i>Login</a>
+              </li>
+
+          </ul>
+
+        </div>
+      </nav>
+
+ <div class="card ">
+   <div class="card-header">
+          <h3 class='text-center'>User Registration</h3>
+        </div>
+        <div class="cad-body">
+            <div style="width:600px; margin:0px auto">
+
+            <form class="" action="register-user.php" method="post">
+                <div class="form-group pt-3">
+                  <label for="name">Your name</label>
+                  <input type="text" name="name"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="username">Your username</label>
+                  <input type="text" name="username"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="email">Email address</label>
+                  <input type="email" name="email"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="mobile">Mobile Number</label>
+                  <input type="text" name="mobile"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" name="password" class="form-control">
+                  <input type="hidden" name="roleid" value="3" class="form-control">
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="register" class="btn btn-success">Register</button>
+                </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+      <?php include 'inc/footer.php' ?>
