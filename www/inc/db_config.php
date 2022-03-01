c9d0907834a56985c41826a5b4f1729a77fad393
Santosh Tamang <santoshtamang@st161726-workStation.local>
2022-03-01 20:26:08 +1100

    changes to databse

--- www/inc/db_config.php
+++ www/inc/db_config.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "admin";
-$password = "Samsung@123";
+$username = "root";
+$password = "root";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=users", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=database0", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
