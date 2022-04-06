1252440fc699cc316323937b57178a167a79c436
Rupinder Kaur Sidhu <roop@Rupinders-MacBook-Pro.local>
2022-04-06 16:56:32 +1000

    file added

--- www/inc/db_config.php
+++ www/inc/db_config.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "dbuser";
-$password = "Microsoft@143Apple";
+$username = "Roop";
+$password = "roop";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=db", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=newdb", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
